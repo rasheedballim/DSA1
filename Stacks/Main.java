55a072250ff2260dcc2e30ee503d1a91c250601b
Rasheed Ballim <rasheedballim1@gmail.com>
2024-06-12 17:40:31 +0200

    init

---
+++ Stacks/Main.java
@@ -0,0 +1,12 @@
+package Stacks;
+
+public class Main {
+    public static void main(String[] args){
+        Stack s =new Stack(4);
+        
+        System.out.println(s.isEmpty());
+        System.out.println(s.isFull());
+
+    }
+    
+}
--- Stacks/Stack.java
+++ Stacks/Stack.java
@@ -1,12 +1,25 @@
 package Stacks;
+
 public class Stack{
     int[] array;
     int topOfStack;
 
-    public Stack(int size){
+     public Stack(int size){
         this.array = new int[size];
         this.topOfStack =-1;
         System.out.println("The Stack has been created with a size of:"+ size);
+    }
+    public boolean isEmpty(){
+        if (topOfStack == -1){
+            return true;
+        }
+        return false;
+    }
+    public boolean isFull(){
+        if (topOfStack == array.length-1){
+            return true;
+        }
+        return false;
     }
     
 }
